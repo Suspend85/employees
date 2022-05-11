b12ea1d9fcd346ce039f96795981503fcc1414a9
Dmitrii <melomanko@gmail.com>
2022-05-11 16:45:50 +0700

    126

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+import './search-panel.css';
+
+const Searchpanel = () => {
+	return <input type="text" className="form-control search-input" placeholder="Найти сотрудника" />;
+};
+
+export default Searchpanel;
