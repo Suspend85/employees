ad95510f8c5f2db21392f974fe010fc403e987d8
Dmitrii <melomanko@gmail.com>
2022-05-12 13:13:17 +0700

    employees deleting added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,19 +8,41 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-	return (
-		<div className="app">
-			<AppInfo />
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: 'John C.', salary: 800, increase: false, id: 1 },
+				{ name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+				{ name: 'Carl W.', salary: 15000, increase: false, id: 3 },
+			],
+		};
+		this.maxId = 4;
+	}
 
-			<div className="search-panel">
-				<SearchPanel />
-				<AppFilter />
+	deleleItem = (id) => {
+		this.setState(({ data }) => {
+			return {
+				data: data.filter((item) => item.id !== id),
+			};
+		});
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<AppInfo />
+
+				<div className="search-panel">
+					<SearchPanel />
+					<AppFilter />
+				</div>
+				<EmployeesList data={this.state.data} onDelete={this.deleleItem} />
+				<EmployeesAddForm />
 			</div>
-			<EmployeesList />
-			<EmployeesAddForm />
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
--- src/components/emloyees-list/employees-list.js
+++ src/components/emloyees-list/employees-list.js
@@ -1,14 +1,16 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
-	return (
-		<ul className="app-list list-group">
-			<EmployeesListItem />
-			<EmployeesListItem />
-			<EmployeesListItem />
-		</ul>
-	);
+const EmployeesList = ({ data, onDelete }) => {
+	const elements = data.map((item) => {
+		const { id, ...itemProps } = item;
+		return <EmployeesListItem
+			key={id}
+			{...itemProps}
+			onDelete={() => onDelete(id)}/>;
+	});
+
+	return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
